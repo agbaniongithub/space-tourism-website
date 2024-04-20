d1d8f9f720404823f5cdbe9808a814b86b0cc341
David Agbaniyaka <fiyin.agba@gmail.com>
2024-04-19 21:36:52 -0400

    ES Lint Rule added, in esli nt

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
       <div className=' flex flex-col items-center lg:items-start gap-4 md:gap-6 lg:gap-[24px] lg:text-left'>
         <p className='text-design-2 font-barlow_condensed tracking-widest text-base md:text-[20px] lg:text-[20px] md:tracking-[3.38px] lg:tracking-[4.72px]'>SO, YOU WANT TO TRAVEL TO</p>
         <p className='text-design-3 text-[80px] lg:text-[150px] font-bellefair'>SPACE</p>
-        <p className='text-design-2 font-barlow text-[15px] md:text-[16px] lg:text-[18px] text-center lg:text-left leading-7 lg:max-w-[450px]'>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+        <p className='text-design-2 font-barlow text-[15px] md:text-[16px] lg:text-[18px] text-center lg:text-left leading-7 lg:leading-[32px] lg:max-w-[450px]'>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
       </div>
       <div className='max-lg:mx-auto rounded-[50%] bg-design-3 h-[150px] w-[150px] md:h-[242px] md:w-[242px] flex items-center justify-center mt-[10rem]'>
         <p className='font-bellefair text-xl md:text-3xl tracking-widest '>EXPLORE</p>
