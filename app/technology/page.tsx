dc2e3afad10c39f4819ec48c47c15b6c4c5ec14c
David Agbaniyaka <fiyin.agba@gmail.com>
2024-04-21 06:05:02 -0400

    Adjusted Technology Page

--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const FirstTechnology = () => {
   return (
-    <div className='px-[24px] text-center lg:text-left lg:max-w-[600px] lg:px-0 lg:ml-[-300px]'>
+    <div className='px-[24px] text-center lg:text-left'>
       <h4 className='text-design-2 font-barlow_condensed text-[14px] md:text-[16px] lg:text-[18px] tracking-[2.36px] mb-[9px]'>THE TERMINOLOGY ...</h4>
       <p className='text-[24px] font-bellefair text-design-3 mb-4 md:text-[40px] lg:text-[60px]'>LAUNCH VEHICLE</p>
       <p className='text-design-2 leading-[25px] lg:leading-[32px] font-barlow text-[15px] md:text-[16px] lg:text-[18px] pb-10'>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth&apos;s surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it&apos;s quite an awe-inspiring sight on the launch pad!</p>
